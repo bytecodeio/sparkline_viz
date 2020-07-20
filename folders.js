3025d9c9e5f865545db457c5d85ca3ecdfe68156
Colin Roy-Ehri <colin.roy.ehri@bytecode.io>
2020-07-20 09:18:38 -0600

    2

---
+++ folders.js
@@ -0,0 +1,48 @@
+looker.plugins.visualizations.add({
+    create: function(element, config) {
+    // Insert a <style> tag with some styles we'll use later.
+    var css = element.innerHTML = `
+      <style>
+        .embed-folder-vis {
+          display: flex;
+          flex-direction: row;
+          flex-wrap: wrap;
+          align-items: flex-start;
+        }
+        a:link {
+          text-decoration: none
+        }
+        a:hover {
+          border: 1px solid #020202;
+          }
+      </style>
+    `;
+  
+    // Create a container element to let us center the text.
+    var container = element.appendChild(document.createElement("div"));
+    container.className = "embed-folder-vis";
+  
+    // Create an element to contain the text.
+    this._textElement = container.appendChild(document.createElement("div"));
+  
+    },
+   updateAsync: function(data, element, config, queryResponse, details, done) {
+  
+      // Grab the first cell of the data.
+      var firstRow = data[0];
+      var firstCell = firstRow[queryResponse.fields.dimensions[0].name];
+  
+      var returnString = `<div class='embed-folder-vis'>`
+      data.forEach((x) =>  {
+          returnString = returnString + `<div style='margin:16px; line-height: 38px; font-family:"Open Sans", "Noto Sans JP", "Noto Sans", "Noto Sans CJK KR", Helvetica, Arial, sans-serif;'> ${x['spaces.name_button'].html} </div>`
+      })
+      
+      returnString = returnString + `</div>`
+  
+      // Insert the data into the page.
+      this._textElement.innerHTML = returnString;
+  
+      // Always call done to indicate a visualization has finished rendering.
+      done()
+    }
+  })
--- sparkline.js
+++ sparkline.js
@@ -1,80 +1,48 @@
 looker.plugins.visualizations.add({
-    // Id and Label are legacy properties that no longer have any function besides documenting
-    // what the visualization used to have. The properties are now set via the manifest
-    // form within the admin/visualizations page of Looker
-    id: "hello_world",
-    label: "Hello World",
-    options: {
-      font_size: {
-        type: "string",
-        label: "Font Size",
-        values: [
-          {"Large": "large"},
-          {"Small": "small"}
-        ],
-        display: "radio",
-        default: "large"
+  create: function(element, config) {
+  // Insert a <style> tag with some styles we'll use later.
+  var css = element.innerHTML = `
+    <style>
+      .embed-folder-vis {
+        display: flex;
+        flex-direction: row;
+        flex-wrap: wrap;
+        align-items: flex-start;
       }
-    },
-    // Set up the initial state of the visualization
-    create: function(element, config) {
-  
-      // Insert a <style> tag with some styles we'll use later.
-      element.innerHTML = `
-        <style>
-          .hello-world-vis {
-            /* Vertical centering */
-            height: 100%;
-            display: flex;
-            flex-direction: column;
-            justify-content: center;
-            text-align: center;
-          }
-          .hello-world-text-large {
-            font-size: 72px;
-          }
-          .hello-world-text-small {
-            font-size: 18px;
-          }
-        </style>
-      `;
-  
-      // Create a container element to let us center the text.
-      var container = element.appendChild(document.createElement("div"));
-      container.className = "hello-world-vis";
-  
-      // Create an element to contain the text.
-      this._textElement = container.appendChild(document.createElement("div"));
-  
-    },
-    // Render in response to the data or settings changing
-    updateAsync: function(data, element, config, queryResponse, details, done) {
-  
-      // Clear any errors from previous updates
-      this.clearErrors();
-  
-      // Throw some errors and exit if the shape of the data isn't what this chart needs
-      if (queryResponse.fields.dimensions.length == 0) {
-        this.addError({title: "No Dimensions", message: "This chart requires dimensions."});
-        return;
+      a:link {
+        text-decoration: none
       }
-  
-      // Grab the first cell of the data
-      var firstRow = data[0];
-      var firstCell = firstRow[queryResponse.fields.dimensions[0].name];
-  
-      // Insert the data into the page
-      this._textElement.innerHTML = LookerCharts.Utils.htmlForCell(firstCell);
-  
-      // Set the size to the user-selected size
-      if (config.font_size == "small") {
-        this._textElement.className = "hello-world-text-small";
-      } else {
-        this._textElement.className = "hello-world-text-large";
-      }
-  
-      // We are done rendering! Let Looker know.
-      done()
-    }
-  });
-  
+      a:hover {
+        border: 1px solid #020202;
+        }
+    </style>
+  `;
+
+  // Create a container element to let us center the text.
+  var container = element.appendChild(document.createElement("div"));
+  container.className = "embed-folder-vis";
+
+  // Create an element to contain the text.
+  this._textElement = container.appendChild(document.createElement("div"));
+
+  },
+ updateAsync: function(data, element, config, queryResponse, details, done) {
+
+    // Grab the first cell of the data.
+    var firstRow = data[0];
+    var firstCell = firstRow[queryResponse.fields.dimensions[0].name];
+
+    var returnString = `<div class='embed-folder-vis'>`
+    data.forEach((x) =>  {
+        returnString = returnString + `<div style='margin:16px; line-height: 38px; font-family:"Open Sans", "Noto Sans JP", "Noto Sans", "Noto Sans CJK KR", Helvetica, Arial, sans-serif;'> ${x['spaces.name_button'].html} </div>`
+    })
+    
+    returnString = returnString + `</div>`
+
+    // Insert the data into the page.
+    this._textElement.innerHTML = returnString;
+
+    // Always call done to indicate a visualization has finished rendering.
+    done()
+  }
+})
